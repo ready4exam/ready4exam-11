645a293a8147f47c0ed0caf5e58e7176e3d19b79
ready4exam <ready4urexam@gmail.com>
2025-11-29 06:24:54 +0530

    🔄 Update table_id for "Linear Inequalities" → linear_inequalities_11_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -289,7 +289,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Linear Inequalities",
-        "table_id": "Ch 6",
+        "table_id": "linear_inequalities_11_quiz",
         "section": "Science/Commerce"
       },
       {
